f3ffe4f40fbe3cbc6200c58f74bd1b38e507aff7
Juan Pinol <JuannnCodes@gmail.com>
2022-07-04 17:02:15 -0400

    Created a database file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = {}
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/server/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js'
+  }
+}
